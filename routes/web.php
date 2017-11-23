8d37c0b2ff964a69f553a19abdb73c1e250d41c8
Max <ilovesamp33@gmail.com>
2017-11-23 23:17:54 +0200

    Added finished authorization

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('student', 'StudentPageController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UserController');
+Route::resource('roles', 'RoleController');
+Route::resource('permissions', 'PermissionController');
