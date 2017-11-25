b66e56d9ff974c0a4fda496a72a44b3a0245bee1
Viacheslav <viacheslav.sheviakov@nure.ua>
2017-11-25 20:52:29 +0200

    Fixed student page view

--- resources/views/student/page.blade.php
+++ resources/views/student/page.blade.php
@@ -1,63 +1,85 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Студент</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Студент</h3></div>
 
-                <div class="panel-body">
-                    <h2>{{ $data[0]->Surname }} {{ $data[0]->Name }}</h2>
-                    <h4>{{ $data[0]->GroupShortTitle }}</h4>
-                    <hr>
-                    <h3>Рассписание</h3>
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
+                    <div class="panel-body">
+                        <h3>{{ $data[0]->Surname }} {{ $data[0]->Name }}</h3>
+                        <h4>Группа: {{ $data[0]->GroupShortTitle }}</h4>
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Оценки</h3></div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr class="small">
                                 <th>Дата</th>
-                                <th>Номер Пары</th>
                                 <th>Дисциплина</th>
-                                <th>Тип занятия</th>
+                                <th>Преп.</th>
+                                <th>Оценка</th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($data[1] as $element)
+                            </thead>
+                            <tbody>
+                            @foreach($data[0]->grades as $grade)
                                 <tr>
-                                    <td>{{ $element->LessonDate }}</td>
-                                    <td>{{ $element->LessonNumber }}</td>
-                                    <td>{{ $element->SubjectShortTitle }}</td>
-                                    <td>{{ $element->LessonType }}</td>
+                                    <td>{{ $grade->schedule->LessonDate }}</td>
+                                    <td>{{ $grade->schedule->teaching->SubjectShortTitle }}</td>
+                                    <td>
+                                        {{ $grade->schedule->teaching->professor->Surname }}<br>
+                                        {{ $grade->schedule->teaching->professor->Name }}<br>
+                                        {{ $grade->schedule->teaching->professor->Patronymic }}
+                                    </td>
+                                    <td>{{ $grade->Grade }}</td>
                                 </tr>
                             @endforeach
-                        </tbody>
-                    </table>
-                    <br>
-                    <h3>Оценки</h3>
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>Дата</th>
-                            <th>Дисциплина</th>
-                            <th>Преподователь</th>
-                            <th>Оценка</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($data[2] as $element)
-                            <tr>
-                                <td>{{ $element->LessonDate }}</td>
-                                <td>{{ $element->SubjectShortTitle }}</td>
-                                <td>{{ $element->Surname }} {{ $element->Name }} {{ $element->Patronymic }}</td>
-                                <td>{{ $element->Grade }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Рассписание</h3></div>
+
+                    <div class="panel-body">
+                        <div class="schedule">
+                            @foreach($data[1] as $date => $lessons)
+                                <div class="day">
+                                    <h3>{{ $date }}</h3>
+                                    @for($i = 0; $i < 8; $i++)
+                                        <div class="lesson">
+                                            @if(isset($lessons[$i]))
+                                                <span class="lesson-type">{{ $lessons[$i][0] }}</span>
+                                                <span class="subject-title">{{ $lessons[$i][1] }}</span>
+                                            @endif
+                                        </div>
+                                    @endfor
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Отчёт</h3></div>
+
+                    <div class="panel-body">
+                        <form method="POST" action="report">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="stuid" id="stuid" value="{{ $data[0]->RecordBookId }}">
+                            <input type="submit" class="btn btn-primary" value="Microsoft Word">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
-
