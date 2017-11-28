1e2fb2ff355c45f77bf27ed4eb32597b565f9d50
Viacheslav <viacheslav.sheviakov@nure.ua>
2017-11-28 23:42:22 +0200

    No

--- app/Http/Controllers/TeacherRoomController.php
+++ app/Http/Controllers/TeacherRoomController.php
@@ -52,7 +52,7 @@ class TeacherRoomController extends Controller
     public function grades(Request $request)
     {
         $scheduleid = $request->input('sid');
-
+        
         $grades = Grade::all()->where('ScheduleId',$scheduleid);
         $group = Schedule::all()->where('ScheduleId', $scheduleid)->pluck('GroupShortTitle')->first();
         $date = Schedule::all()->where('ScheduleId', $scheduleid)->pluck('LessonDate')->first();
